020ac2a4c50fc51e2e6e2adb36e3112d98155ecb
Juan-Salvador Alcorta <156425908+jsac2001@users.noreply.github.com>
2025-01-16 10:32:32 +0100

    ajout de la configuration pour le dir pisse mais 2

--- events/pisse.js
+++ events/pisse.js
@@ -3,14 +3,18 @@ const { Events } = require("discord.js");
 module.exports = {
   name: Events.MessageCreate,
   execute(message) {
-    const triggerWords = ["Quentin", "Ali", "Juan"];
+    const triggerWords = ["Quentin", "Juan", "Ali"];
 
     if (message.author.bot) return false;
 
     triggerWords.forEach((word) => {
       if (message.content.includes(word)) {
-        message.reply("Ta gueule !");
+        message.reply("Est un déchet");
       }
     });
+
+    if (message.author.id == 142335378064408585) {
+      message.reply("Ta gueule");
+    }
   },
 };
